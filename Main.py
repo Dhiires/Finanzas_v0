d56da3e379ccb42b9aaa2ae883b6f87067c373db
jose <jose.barriosr@sansano.usm.cl>
2024-12-15 16:25:43 -0300

    insert purchase online

--- Main.py
+++ Main.py
@@ -5,4 +5,4 @@ new_entry = UserInput.get_purchase_data_from_user()
 
 print(f"Descripción compra: {new_entry.description}\nMonto: {new_entry.amount}\nCuotas: {new_entry.quota}\nDía: {new_entry.day}\nMes: {new_entry.month}\nAño: {new_entry.year}")
 
-print(FilesManagement.read_excel(new_entry))
+FilesManagement.insert_purchase_into_excel(new_entry)
