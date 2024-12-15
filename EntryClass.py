3ba9f76d35024b97f71ef409c08902d65532aa0e
jose <jose.barriosr@sansano.usm.cl>
2024-12-15 16:33:53 -0300

    empty cases for amount and quota

--- EntryClass.py
+++ EntryClass.py
@@ -4,8 +4,8 @@ class Purchase:
     
     def __init__(self, description, amount, quota, day, month, year) -> None:
         self.description = description
-        self.amount = amount
-        self.quota = quota
+        self.amount = amount if amount != "" else 0
+        self.quota = quota if quota != "" else 1
         self.day = datetime.today().strftime("%d") if day == "" else day
         self.month = datetime.today().strftime("%m") if month == "" else month
         self.year = datetime.today().strftime("%Y") if year == "" else year
