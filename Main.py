df730c27422bc5042a85d5ec92bbcfdae8b48b17
jose <jose.barriosr@sansano.usm.cl>
2024-12-15 16:27:32 -0300

    print format changed

--- Main.py
+++ Main.py
@@ -3,6 +3,6 @@ import FilesManagement
 
 new_entry = UserInput.get_purchase_data_from_user()
 
-print(f"Descripción compra: {new_entry.description}\nMonto: {new_entry.amount}\nCuotas: {new_entry.quota}\nDía: {new_entry.day}\nMes: {new_entry.month}\nAño: {new_entry.year}")
+print(f"Descripción compra: {new_entry.description}\nMonto: {new_entry.amount}\nCuotas: {new_entry.quota}\nFecha: {new_entry.day}-{new_entry.month}-{new_entry.year}")
 
 FilesManagement.insert_purchase_into_excel(new_entry)
