ef2358e284eb4ded7ca2c035a806eb6424d9229d
jose <jose.barriosr@sansano.usm.cl>
2024-12-15 15:47:17 -0300

    added date to class

--- EntryClass.py
+++ EntryClass.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
 class Purchase:
     
-    def __init__(self, description, amount, quota) -> None:
+    def __init__(self, description, amount, quota, day, month, year) -> None:
         self.description = description
         self.amount = amount
         self.quota = quota
+        self.day = datetime.today().strftime("%d") if day == "" else day
+        self.month = datetime.today().strftime("%m") if month == "" else month
+        self.year = datetime.today().strftime("%Y") if year == "" else year
