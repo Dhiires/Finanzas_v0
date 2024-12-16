d80bf0d3e1acf5abb79edf0b35202904f4e4b163
jose <jose.barriosr@sansano.usm.cl>
2024-12-15 23:22:08 -0300

    now in .exe

--- Main.py
+++ Main.py
@@ -6,3 +6,5 @@ new_entry = UserInput.get_purchase_data_from_user()
 print(f"Descripción compra: {new_entry.description}\nMonto: {new_entry.amount}\nCuota actual: {new_entry.quota}\nCuotas: {new_entry.payments}\nFecha: {new_entry.day}-{new_entry.month}-{new_entry.year}")
 
 FilesManagement.insert_purchase_into_excel(new_entry)
+
+input("Presiona Enter para finalizar...")
