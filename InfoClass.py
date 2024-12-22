ddad799feb446013f7ff0f32d3ac7d2793ed5abe
jose <jose.barriosr@sansano.usm.cl>
2024-12-22 19:24:34 -0300

    initial commit

---
+++ InfoClass.py
@@ -0,0 +1,18 @@
+class Bill:
+    
+    def __init__(self, description, amount, individual) -> None:
+        self.description = description
+        self.amount = amount
+        self.individual = individual
+        
+    @classmethod
+    def fetch_bill_info_from_dataframe(cls, df):
+        bill_list = []
+        for _, row in df.iterrow():
+            bill = cls(
+                description = row.get("Descripcion",""),
+                amount = row.get("Monto",""),
+                individual = row.get("Individual",True)
+                )
+            bill_list.append(bill)
+        return bill_list
